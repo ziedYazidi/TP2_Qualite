7c00a7852730e156dca84589dbbe9fa8d0562010
ziedYazidi <ziedyazidi75@gmail.com>
2017-11-23 21:25:30 +0100

    Second commit

--- src/main/java/client/App.java
+++ src/main/java/client/App.java
@@ -1,10 +1,16 @@
 package client;
 
+import entities.Compte;
+
+import java.util.Date;
+
 /**
  * Created by zied on 22/11/2017.
  */
 public class App {
     public static void main(String[] args){
+        Compte compte = new Compte(1,new Date(10,12,2017),"Zied yazidi",1000);
+        System.out.println(compte.getOwnerCompte());
 
     }
 }
